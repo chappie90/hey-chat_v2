b9aa3ff77ac882fa7bcd46e86882a530d33ce650
Stoyan Garov <stoyan@3bweb.com>
2020-09-27 11:56:13 +0100

    Add custom tabs bottom tab navigator

--- mobile-client/src/navigation/MainFlow.tsx
+++ mobile-client/src/navigation/MainFlow.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 import ChatsScreen from '../screens/ChatsScreen';
 import CurrentChatScreen from '../screens/CurrentChatScreen';
 import ContactsScreen from '../screens/ContactsScreen';
 import ProfileScreen from '../screens/ProfileScreen';
-import CustomTabBar from './CustomTabBar';
+import { Colors, Headings } from '../variables/variables';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -33,16 +35,48 @@ const ChatsStackNavigator = () => {
 
 const MainFlow = () => {
   return (
-    <Tab.Navigator tabBar={props => <MyTabBar {...props} />}>
+    <Tab.Navigator 
+      initialRouteName="Chats"
+      tabBarOptions={{
+        style: {
+          backgroundColor: Colors.darkGrey
+        },
+        tabStyle: {
+          paddingVertical: 4,
+        },
+        activeTintColor: Colors.primary,
+        inactiveTintColor: Colors.white
+      }}
+    >
       <Tab.Screen 
-        name="ChatsStackNavigator" 
+        name="Chats" 
         component={ChatsStackNavigator} 
         options={({ route }) => ({
-          tabBarVisible: hideTabBar(route)
+          tabBarVisible: hideTabBar(route),
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialIcon name="chat" size={22} color={focused ? Colors.primary : Colors.white} />
+          ),
+          tabBarBadge: 3,
         })}
       />
-      <Tab.Screen name="Contacts" component={ContactsScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen 
+        name="Contacts" 
+        component={ContactsScreen}
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <FontAwesome5 name="user-friends" size={22} color={focused ? Colors.primary : Colors.white} />
+          )
+        }}
+      />
+      <Tab.Screen 
+        name="Profile" 
+        component={ProfileScreen} 
+        options={{
+          tabBarIcon: ({ color, size, focused }) => (
+            <MaterialIcon name="account-balance" size={22} color={focused ? Colors.primary : Colors.white} />
+          )
+        }}
+      />
     </Tab.Navigator>
   );
 };
