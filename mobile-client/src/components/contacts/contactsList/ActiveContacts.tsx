b73f596acd26011d67c008b00de6087fa3cf64de
Stoyan Garov <stoyan@3bweb.com>
2020-10-03 18:10:29 +0100

    Add all users and active users list components

--- mobile-client/src/components/contacts/contactsList/ActiveContacts.tsx
+++ mobile-client/src/components/contacts/contactsList/ActiveContacts.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { 
+  View,
   StyleSheet,
-  ScrollView,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Image,
+  RefreshControl,
+  FlatList
 } from 'react-native';
 
-import { Colors, Headings } from '../../../variables/variables';
+import { Images } from '../../../../assets/assets';
+import { Colors, Fonts, Headings } from '../../../variables/variables';
+import CustomText from '../../../components/CustomText';
 
 type Contact = {
   _id: number;
@@ -13,20 +19,93 @@ type Contact = {
 };
 
 type ActiveContactsProps = {
-  searchResults: Contact[];
-  onAddContact: (contactId: number) => void;
+  contacts: Contact[];
+  activeUsersCount: number;
 };
 
-const ActiveContacts = ({  }: ActiveContactsProps) => {
-  return (
-    <ScrollView style={styles.scrollContainer}>
+const ActiveContacts = ({ contacts, activeUsersCount }: ActiveContactsProps) => {
 
-    </ScrollView>
+  if (activeUsersCount === 0) {
+    return <CustomText style={styles.noResults}>No active users</CustomText>;
+  }
+
+  return (
+    <FlatList
+      // refreshControl={
+      //   <RefreshControl
+      //     onRefresh={() => getContacts({ username })}
+      //     refreshing={isLoading}
+      //     tintColor={Colors.primary} />
+      // }
+      data={contacts}
+      //   onEndReached={() => {
+      //   if (localContacts.length < contacts.length) {
+      //     loadMoreContacts();
+      //   }
+      // }}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={({ item, index }) => {
+        // const chat = previousChats.filter(c => c.contact === rowData.item.user.username);
+        return (
+          <TouchableWithoutFeedback 
+            onPress={() => {
+              // navigation.navigate('ChatDetail', {
+              //   username: rowData.item.user.username,
+              //   image: rowData.item.user.profile ? rowData.item.user.profile.cloudinaryImgPath_150 : '',
+              //   type: 'private',
+              //   chatId: chat.length > 0 ? chat[0].chatId : null,
+              //   origin: 'ContactsList'
+              // })
+            }}>
+            <View 
+              style={styles.row}
+            >
+              <View style={{ overflow: 'hidden', backgroundColor: '#F0F0F0', width: 44, height: 44, borderRadius: 22}}>
+                {item.profile ?
+                  <Image 
+                    style={{ width: '100%', height: '100%' }} 
+                    placeholderStyle={styles.placeholder}
+                    source={{ uri: item.profile.cloudinaryImgPath_150 }}
+                  /> : 
+                  <Image 
+                    style={{ width: '100%', height: '100%' }} 
+                    source={ Images.avatarSmall } 
+                  />
+                }
+              </View>                  
+              <View style={styles.itemContainer}>
+                <CustomText style={styles.name} fontWeight={Fonts.semiBold}>
+                  {item.username}
+                </CustomText>
+              </View>
+              {/* {onlineContacts.includes(rowData.item.username) && (
+                <Badge
+                  badgeStyle={styles.badge}
+                  containerStyle={styles.badgeContainer}
+                />
+              )}   */}
+            </View>
+          </TouchableWithoutFeedback>
+        );
+      }}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center', 
+    paddingVertical: 6, 
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.lightGrey,
+    backgroundColor: Colors.white
+  },
+  noResults: {
+    textAlign: 'center',
+    marginTop: 15
+  }
 });
 
 export default ActiveContacts;
--- mobile-client/src/components/contacts/contactsList/AllContacts.tsx
+++ mobile-client/src/components/contacts/contactsList/AllContacts.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { 
+  View,
   StyleSheet,
-  ScrollView,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Image,
+  RefreshControl
 } from 'react-native';
+import { SwipeListView } from 'react-native-swipe-list-view';
+import EntypoIcon from 'react-native-vector-icons/Entypo';
 
-import { Colors, Headings } from '../../../variables/variables';
-import SearchItem from './SearchItem';
+import { Images } from '../../../../assets/assets';
+import { Colors, Fonts, Headings } from '../../../variables/variables';
+import CustomText from '../../../components/CustomText';
 
 type Contact = {
   _id: number;
@@ -14,19 +20,109 @@ type Contact = {
 };
 
 type AllContactsProps = {
-
+  contacts: Contact[];
 };
 
-const AllContacts = ({  }: AllContactsProps) => {
+const AllContacts = ({ contacts }: AllContactsProps) => {
   return (
-    <ScrollView style={styles.scrollContainer}>
-
-    </ScrollView>
+    <SwipeListView
+      // refreshControl={
+      //   <RefreshControl
+      //     onRefresh={() => getContacts({ username })}
+      //     refreshing={isLoading}
+      //     tintColor={Colors.primary} />
+      // }
+      disableRightSwipe
+      data={contacts}
+      //   onEndReached={() => {
+      //   if (localContacts.length < contacts.length) {
+      //     loadMoreContacts();
+      //   }
+      // }}
+      keyExtractor={(item, index) => index.toString()}
+      renderItem={ (rowData, rowMap) => {
+        // const chat = previousChats.filter(c => c.contact === rowData.item.user.username);
+        return (
+          <TouchableWithoutFeedback 
+            onPress={() => {
+              // navigation.navigate('ChatDetail', {
+              //   username: rowData.item.user.username,
+              //   image: rowData.item.user.profile ? rowData.item.user.profile.cloudinaryImgPath_150 : '',
+              //   type: 'private',
+              //   chatId: chat.length > 0 ? chat[0].chatId : null,
+              //   origin: 'ContactsList'
+              // })
+            }}>
+            <View 
+              style={styles.rowFront}
+            >
+              <View style={{ overflow: 'hidden', backgroundColor: '#F0F0F0', width: 44, height: 44, borderRadius: 22}}>
+                {rowData.item.profile ?
+                  <Image 
+                    style={{ width: '100%', height: '100%' }} 
+                    placeholderStyle={styles.placeholder}
+                    source={{ uri: rowData.item.profile.cloudinaryImgPath_150 }}
+                  /> : 
+                  <Image 
+                    style={{ width: '100%', height: '100%' }} 
+                    source={ Images.avatarSmall } 
+                  />
+                }
+              </View>                  
+              <View style={styles.itemContainer}>
+                <CustomText style={styles.name} fontWeight={Fonts.semiBold}>
+                  {rowData.item.username}
+                </CustomText>
+              </View>
+              {/* {onlineContacts.includes(rowData.item.username) && (
+                <Badge
+                  badgeStyle={styles.badge}
+                  containerStyle={styles.badgeContainer}
+                />
+              )}   */}
+            </View>
+          </TouchableWithoutFeedback>
+        );
+      }}
+      renderHiddenItem={ (rowData, rowMap) => (
+        <View style={styles.rowBack}>
+          <TouchableOpacity style={{ }} onPress={() => {}}>
+            <View style={styles.deleteIcon}>
+              <EntypoIcon name="trash" size={24} color={Colors.white} />
+            </View>
+          </TouchableOpacity>
+        </View>
+      )}
+      rightOpenValue={-65}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-
+  rowFront: {
+    flexDirection: 'row',
+    alignItems: 'center', 
+    paddingVertical: 6, 
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.lightGrey,
+    backgroundColor: Colors.white
+  },
+  rowBack: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  deleteIcon: {
+    backgroundColor: Colors.tertiary,
+    width: 44,
+    height: 44,
+    borderRadius: 22,
+    marginHorizontal: 10,
+    alignItems: 'center',
+    justifyContent: 'center' 
+  }
 });
 
 export default AllContacts;
