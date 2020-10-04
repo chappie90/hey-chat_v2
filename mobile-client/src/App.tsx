d7c9cdcc88a4dee6011e570ed4f265e1c93dbf03
Stoyan Garov <stoyan@3bweb.com>
2020-10-04 20:27:12 +0100

    Add event listener to detect app state changes

--- mobile-client/src/App.tsx
+++ mobile-client/src/App.tsx
@@ -1,22 +1,51 @@
 import 'react-native-gesture-handler';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
-import io from 'socket.io-client';
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  StyleSheet, 
+  View, 
+  AppState
+} from 'react-native';
 
 import Navigator from './navigation/Navigator';
+import { socket } from './socket/socket';
 import { Provider as AuthProvider } from './context/AuthContext';
 import { Provider as ContactsProvider } from './context/ContactsContext';
 import SplashScreen from 'react-native-splash-screen'
 
 declare const global: {HermesInternal: null | {}};
 
+// Socket connection
+socket.on('connect', () => {
+  console.log('socket connected');
+});
+
+socket.on('disconnect', () => {
+  console.log('socket disconnected');
+});
+
 const App = () => {
+  const appState = useRef<string>(AppState.currentState);
+
+  const handleAppStateChange = (nextAppState: string) => {
+    if (
+      appState.current.match(/inactive|background/) && 
+      nextAppState === 'active'
+    ) {
+      console.log('App has come to foreground');
+    }
+
+    appState.current = nextAppState;
+    console.log('App state: ' + appState.current);
+  };
+
   useEffect(() => {
     SplashScreen.hide();
-    const socket = io('http://localhost:3006');
-    socket.on('message', (message: string) => {
-      console.log(message);
-    });
+
+    AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    };
   }, [])
 
   return (
