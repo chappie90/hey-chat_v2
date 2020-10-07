ecdb5fffdc8e9ab17c91937680a9ba84a54c10e4
Stoyan Garov <stoyan@3bweb.com>
2020-10-08 00:37:33 +0100

    Get last message for each chat

--- server/src/controllers/ChatsController.ts
+++ server/src/controllers/ChatsController.ts
@@ -2,18 +2,37 @@ import { Request, Response, NextFunction } from 'express';
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const Chat = mongoose.model('Chat');
+const Message = mongoose.model('Message');
 
 const getChats = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { userId } = req.query;
 
+  let chats,
+      unreadMessagesCount: number;
+
   try {
     const user = await User.find(
       { _id: userId }
-    ).populate('chats');
+    ).lean()
+     .populate('chats')
+     .populate({
+       path: 'chats',
+       populate: {
+         path: 'participants',
+         model: 'User',
+         select: '_id username'
+       }
+     });
 
-    const chats = user[0].chats;
+    chats = user[0].chats;
 
-    console.log(chats)
+    // Get last message to be displayed on each chat
+    for (const chat of chats) {
+      const lastMessage = await Message.find({ chat: chat._id })
+        .sort({ 'message.createDate': -1 })
+        .limit(1);
+      chat.lastMessage = lastMessage;
+    } 
 
     res.status(200).send({ chats });
   } catch (err) {
