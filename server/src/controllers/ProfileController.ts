58cd2543e663b07224a34edd5405bf90b970b79b
Stoyan <stoyan.garov@yahoo.com>
2020-11-07 18:26:21 +0000

    Add get and upload profile image controller methods

--- server/src/controllers/ProfileController.ts
+++ server/src/controllers/ProfileController.ts
@@ -2,20 +2,53 @@ import { Request, Response, NextFunction } from 'express';
 const mongoose = require('mongoose');
 
 const User = mongoose.model('User');
-import { TUser } from '../types/index';
 import transformImage from '../helpers/transformImage';
 
 const getImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const { userId } = req.query;
 
+    const user = await User.findOne({ _id: userId });
+
+    res.status(200).send({ profileImage: user.profile.image.medium.path });
+  } catch(err) {
+    console.log(err);
+    next(err);
+  }
 };
 
 const uploadImage = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const image = req.file;
+    const userId = req.body.userId;
 
-    transformImage(image, 'profile', 'small');
+    const profileImgFolder = 'public/uploads/profile';
 
-    res.status(200).send({ placeholder: '' }); 
+    const imageNameOriginal = image.filename;
+    const imageNameSmall = transformImage(image, 'profile', 'small');
+    const imageNameMedium = transformImage(image, 'profile', 'medium');
+
+    await User.updateOne(
+      { _id: userId },
+      { profile: {
+        image: {
+          original: {
+            name: imageNameOriginal,
+            path: `${profileImgFolder}/original/${imageNameOriginal}`
+          },
+          small: {
+            name: imageNameSmall,
+            path: `${profileImgFolder}/small/${imageNameSmall}`
+          },
+          medium: {
+            name: imageNameMedium,
+            path: `${profileImgFolder}/medium/${imageNameMedium}`
+          }
+        }
+      } }
+    );
+
+    res.status(200).send({ profileImage: `${profileImgFolder}/medium/${imageNameMedium}` }); 
   } catch (err) {
     console.log(err);
     next(err);
--- server/src/helpers/transformImage.ts
+++ server/src/helpers/transformImage.ts
@@ -4,7 +4,7 @@ const transformImage = (
   imageFile: Express.Multer.File, 
   destinationFolder: string,
   outputSize: string
-): void => {
+): string => {
   const uploadsFolder = `${global.appRoot}/public/uploads`;
   const originalImageName = imageFile.filename;
 
@@ -12,14 +12,14 @@ const transformImage = (
   if (outputSize === 'small') {
     outputDimensions = [120, 120];
   } else if (outputSize === 'medium') {
-    outputDimensions = [200, 200];
+    outputDimensions = [400, 400];
   }
   
   let splitNameParts = imageFile.filename.split('.');
   const fileExt = splitNameParts[splitNameParts.length - 1];
   splitNameParts.pop();
   const joinNameParts = splitNameParts.join('');
-  const resizedImageName = `${joinNameParts}_small.${fileExt}`;
+  const resizedImageName = `${joinNameParts}_${outputSize}.${fileExt}`;
 
   Jimp.read(`${uploadsFolder}/${destinationFolder}/original/${originalImageName}`)
     .then(file => {
@@ -29,6 +29,8 @@ const transformImage = (
         .write(`${uploadsFolder}/${destinationFolder}/${outputSize}/${resizedImageName}`)
     })
     .catch(err => console.log(err));
+
+  return resizedImageName;
 };
 
 export default transformImage;
