28ae4ded258dc4148c2d83d3a11a2dd929a8285c
Stoyan <stoyan.garov@yahoo.com>
2021-02-02 21:52:17 +0000

    Clean up socket call handlers

--- mobile-client/src/socket/eventHandlers/callHandlers.ts
+++ mobile-client/src/socket/eventHandlers/callHandlers.ts
@@ -128,56 +128,9 @@ const onSdpAnswerReceived = (data: string,  callState: TCall, socketState: any, 
   dispatch(callActions.startCall());
 };
 
-const onCallEnded = async (userId: number, callState: TCall, navigate: any,  dispatch: Dispatch) => {
-  const callId = callState.callId;
-  console.log(callState)
-  console.log('on call ended handler')
-
-
-  dispatch(callActions.endCall());
-
-  RNCallKeep.rejectCall(callId);
-  RNCallKeep.endAllCalls();
-  RNCallKeep.endCall(callId);
-
-  const contact = userId === callState.caller._id ? callState.callee : callState.caller;
-
-  const routeParams = { 
-    chatType: 'private', 
-    chatId: callState.chat.chatId,
-    contactId: contact._id,
-    contactName: contact.username,
-    contactProfile: contact.avatar?.small
-  };
-  navigate('CurrentChat', routeParams);
-};
-
-const onMissedCall = (data: string, username: string, chatHistory: any, dispatch: Dispatch) => {
-  const { chatId, message } = JSON.parse(data);
-  // Fetch chat messages if not loaded yet
-  if (!chatHistory[chatId]) {
-    dispatch(chatsActions.getMessages(username, '', chatId));
-  } else {
-    // Append last message if chat loaded
-    dispatch(chatsActions.addMessage(
-      chatId, 
-      {
-        ...message,
-        sender: {
-          ...message.sender,
-          _id: 2
-        },
-        delivered: true
-      }
-    ));
-  }
-};
-
 export default {
   // onConfirmVoipPushReceived,
   onSdpOfferReceived,
   onICECandidateReceived,
   onSdpAnswerReceived,
-  onCallEnded,
-  onMissedCall
 };
